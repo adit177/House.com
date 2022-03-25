f9fb7cb1ecd23a9611d765234ccb4c6e4703a9c4
Aditya Agnihotri <adityaagnihotri177@gmail.com>
2022-03-26 01:25:34 +0530

    ready for miniproject

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -28,7 +28,7 @@ module.exports.createCampground = async (req, res, next) => {
     campground.author = req.user._id;
     await campground.save();
     console.log(campground);
-    req.flash('success', 'Successfully made a new campground!');
+    req.flash('success', 'Successfully added your House!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
@@ -40,7 +40,7 @@ module.exports.showCampground = async (req, res,) => {
         }
     }).populate('author');
     if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
+        req.flash('error', 'Cannot find that house!');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/show', { campground });
@@ -50,7 +50,7 @@ module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
     if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
+        req.flash('error', 'Cannot find that house!');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/edit', { campground });
@@ -69,13 +69,13 @@ module.exports.updateCampground = async (req, res) => {
         }
         await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
     }
-    req.flash('success', 'Successfully updated campground!');
+    req.flash('success', 'Successfully updated house!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted campground')
+    req.flash('success', 'Successfully deleted your house!')
     res.redirect('/campgrounds');
 }
